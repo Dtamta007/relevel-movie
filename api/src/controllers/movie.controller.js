1f1e39305a87c7165b4427771934201caccc161e
dtamta007 <dtamta007@gmail.com>
2022-03-22 22:28:13 +0530

    Relevel - MovieTime

---
+++ api/src/controllers/movie.controller.js
@@ -0,0 +1,27 @@
+const createMovie = (req,res) =>{
+    //Creating Movie logic here
+};
+
+const getAllVideos = (req,res) =>{
+    //Get all videos both movies and series.
+}
+
+const getAllMovies = (req,res) =>{
+    //Get All the movies
+}
+
+const getAllSeries = (req,res) =>{
+    //Get All the series
+}
+
+const getFeaturedVideo = (req,res) =>{
+    //Get Any faetured video at random
+}
+
+const AdsController = {
+    createMovie,
+    getFeaturedVideo,
+    getAllMovies,
+    getAllSeries,
+    getAllVideos
+}
---
+++ api/src/routes/movies.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const MovieController = require('../controllers/movie.controller');
+
+// Create routes for movies here
+module.exports = router;
